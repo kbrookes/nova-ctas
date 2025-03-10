ea3d34953b3f071fc33ceb8d0413372773723e54
kbrookes <kelsey@thinksync.com.au>
2025-03-10 14:14:24 +1100

    Version 1.1.25: Fixed admin interface styling and tab functionality

--- nova-ctas.php
+++ nova-ctas.php
@@ -3,7 +3,7 @@
  * Plugin Name: Nova CTAs
  * Plugin URI: https://github.com/kbrookes/nova-ctas
  * Description: A WordPress plugin for creating and managing Call To Action buttons
- * Version: 1.1.24
+ * Version: 1.1.25
  * Requires at least: 5.0
  * Requires PHP: 7.2
  * Author: Kelsey Brookes
@@ -13,7 +13,6 @@
  * Text Domain: nova-ctas
  * Domain Path: /languages
  * GitHub Plugin URI: https://github.com/kbrookes/nova-ctas
- * GitHub Branch: main
  * Primary Branch: main
  * Release Asset: true
  */
@@ -24,7 +23,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('NOVA_CTAS_VERSION', '1.1.24');
+define('NOVA_CTAS_VERSION', '1.1.25');
 define('NOVA_CTAS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('NOVA_CTAS_PLUGIN_URL', plugin_dir_url(__FILE__));
 
